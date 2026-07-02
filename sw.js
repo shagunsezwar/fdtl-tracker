8baec5291f78d1e6def1040b9aca976bacdbf05f
shagunsezwar <shagun.sezwar@gmail.com>
2026-07-02 14:19:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fdtl-v57';
+const CACHE = 'fdtl-v58';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon192.png', '/icon512.png'];
 self.addEventListener('install', e => {
   self.skipWaiting();
